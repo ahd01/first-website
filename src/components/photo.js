6207ec5c1d84fab414537f497245ff6075f388a9
ahd-eslam <eslamahd69@gmail.com>
2023-09-10 23:24:11 +0300

    second commit

--- src/components/photo.js
+++ src/components/photo.js
@@ -1,4 +1,4 @@
 const photo =
-  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wired.com%2Fstory%2Fgoogle-photos-alternatives%2F&psig=AOvVaw3g1fpJVLnsR4YNkq_UmW6r&ust=1693675094870000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCMjt6MT1iYEDFQAAAAAdAAAAABAE";
+  "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.wired.com%2Fstory%2Fgoogle-photos-alternatives%2F&psig=AOvVaw3g1fpJVLnsR4YNkq_UmW6r&https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRuUd2Q7_GtYGakSiMRKp0omc13FMflTYARNA&usqp=CAU=1693675094870000&source=images&cd=vfe&opi=89978449&ved=0CBAQjRxqFwoTCMjt6MT1iYEDFQAAAAAdAAAAABAE";
 
 export default photo;
